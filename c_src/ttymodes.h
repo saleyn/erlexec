dfa8313c94c259b2422807d270112cb3fcfe46d1
Serge Aleynikov <saleyn@gmail.com>
2022-07-08 20:14:59 -0400

    Fix alignment

--- c_src/ttymodes.h
+++ c_src/ttymodes.h
@@ -5,115 +5,115 @@
 #define TTY_OP_OSPEED 129
 
 /* name, atom */
-TTYCHAR(VINTR, "vintr")
-TTYCHAR(VQUIT, "vquit")
-TTYCHAR(VERASE, "verase")
+TTYCHAR(VINTR,    "vintr")
+TTYCHAR(VQUIT,    "vquit")
+TTYCHAR(VERASE,   "verase")
 #if defined(VKILL)
-TTYCHAR(VKILL, "vkill")
+TTYCHAR(VKILL,    "vkill")
 #endif /* VKILL */
-TTYCHAR(VEOF, "veof")
+TTYCHAR(VEOF,     "veof")
 #if defined(VEOL)
-TTYCHAR(VEOL, "veol")
+TTYCHAR(VEOL,     "veol")
 #endif /* VEOL */
 #ifdef VEOL2
-TTYCHAR(VEOL2, "veol2")
+TTYCHAR(VEOL2,    "veol2")
 #endif /* VEOL2 */
-TTYCHAR(VSTART, "vstart")
-TTYCHAR(VSTOP, "vstop")
+TTYCHAR(VSTART,   "vstart")
+TTYCHAR(VSTOP,    "vstop")
 #if defined(VSUSP)
-TTYCHAR(VSUSP, "vsusp")
+TTYCHAR(VSUSP,    "vsusp")
 #endif /* VSUSP */
 #if defined(VDSUSP)
-TTYCHAR(VDSUSP, "vdsusp")
+TTYCHAR(VDSUSP,   "vdsusp")
 #endif /* VDSUSP */
 #if defined(VREPRINT)
 TTYCHAR(VREPRINT, "vreprint")
 #endif /* VREPRINT */
 #if defined(VWERASE)
-TTYCHAR(VWERASE, "vwerase")
+TTYCHAR(VWERASE,  "vwerase")
 #endif /* VWERASE */
 #if defined(VLNEXT)
-TTYCHAR(VLNEXT, "vlnext")
+TTYCHAR(VLNEXT,   "vlnext")
 #endif /* VLNEXT */
 #if defined(VFLUSH)
-TTYCHAR(VFLUSH, "vflush")
+TTYCHAR(VFLUSH,   "vflush")
 #endif /* VFLUSH */
 #ifdef VSWTCH
-TTYCHAR(VSWTCH, "vswtch")
+TTYCHAR(VSWTCH,   "vswtch")
 #endif /* VSWTCH */
 #if defined(VSTATUS)
-TTYCHAR(VSTATUS, "vstatus")
+TTYCHAR(VSTATUS,  "vstatus")
 #endif /* VSTATUS */
 #ifdef VDISCARD
 TTYCHAR(VDISCARD, "vdiscard")
 #endif /* VDISCARD */
 
 /* name, field, atom */
-TTYMODE(IGNPAR, c_iflag, "ignpar")
-TTYMODE(PARMRK, c_iflag, "parmrk")
-TTYMODE(INPCK, c_iflag, "inpck")
-TTYMODE(ISTRIP, c_iflag, "istrip")
-TTYMODE(INLCR, c_iflag, "inlcr")
-TTYMODE(IGNCR, c_iflag, "igncr")
-TTYMODE(ICRNL, c_iflag, "icrnl")
+TTYMODE(IGNPAR,  c_iflag, "ignpar")
+TTYMODE(PARMRK,  c_iflag, "parmrk")
+TTYMODE(INPCK,   c_iflag, "inpck")
+TTYMODE(ISTRIP,  c_iflag, "istrip")
+TTYMODE(INLCR,   c_iflag, "inlcr")
+TTYMODE(IGNCR,   c_iflag, "igncr")
+TTYMODE(ICRNL,   c_iflag, "icrnl")
 #if defined(IUCLC)
-TTYMODE(IUCLC, c_iflag, "iuclc")
+TTYMODE(IUCLC,   c_iflag, "iuclc")
 #endif
-TTYMODE(IXON, c_iflag, "ixon")
-TTYMODE(IXANY, c_iflag, "ixany")
-TTYMODE(IXOFF, c_iflag, "ixoff")
+TTYMODE(IXON,    c_iflag, "ixon")
+TTYMODE(IXANY,   c_iflag, "ixany")
+TTYMODE(IXOFF,   c_iflag, "ixoff")
 #ifdef IMAXBEL
 TTYMODE(IMAXBEL, c_iflag, "imaxbel")
 #endif /* IMAXBEL */
 #ifdef IUTF8
-TTYMODE(IUTF8, c_iflag, "iutf8")
+TTYMODE(IUTF8,   c_iflag, "iutf8")
 #endif /* IUTF8 */
 
-TTYMODE(ISIG, c_lflag, "isig")
-TTYMODE(ICANON, c_lflag, "icanon")
+TTYMODE(ISIG,    c_lflag, "isig")
+TTYMODE(ICANON,  c_lflag, "icanon")
 #ifdef XCASE
-TTYMODE(XCASE, c_lflag, "xcase")
+TTYMODE(XCASE,   c_lflag, "xcase")
 #endif
-TTYMODE(ECHO, c_lflag, "echo")
-TTYMODE(ECHOE, c_lflag, "echoe")
-TTYMODE(ECHOK, c_lflag, "echok")
-TTYMODE(ECHONL, c_lflag, "echonl")
-TTYMODE(NOFLSH, c_lflag, "noflsh")
-TTYMODE(TOSTOP, c_lflag, "tostop")
+TTYMODE(ECHO,    c_lflag, "echo")
+TTYMODE(ECHOE,   c_lflag, "echoe")
+TTYMODE(ECHOK,   c_lflag, "echok")
+TTYMODE(ECHONL,  c_lflag, "echonl")
+TTYMODE(NOFLSH,  c_lflag, "noflsh")
+TTYMODE(TOSTOP,  c_lflag, "tostop")
 #ifdef IEXTEN
-TTYMODE(IEXTEN, c_lflag, "iexten")
+TTYMODE(IEXTEN,  c_lflag, "iexten")
 #endif /* IEXTEN */
 #if defined(ECHOCTL)
 TTYMODE(ECHOCTL, c_lflag, "echoctl")
 #endif /* ECHOCTL */
 #ifdef ECHOKE
-TTYMODE(ECHOKE, c_lflag, "echoke")
+TTYMODE(ECHOKE,  c_lflag, "echoke")
 #endif /* ECHOKE */
 #if defined(PENDIN)
-TTYMODE(PENDIN, c_lflag, "pendin")
+TTYMODE(PENDIN,  c_lflag, "pendin")
 #endif /* PENDIN */
 
-TTYMODE(OPOST, c_oflag, "opost")
+TTYMODE(OPOST,   c_oflag, "opost")
 #if defined(OLCUC)
-TTYMODE(OLCUC, c_oflag, "olcuc")
+TTYMODE(OLCUC,   c_oflag, "olcuc")
 #endif
 #ifdef ONLCR
-TTYMODE(ONLCR, c_oflag, "onlcr")
+TTYMODE(ONLCR,   c_oflag, "onlcr")
 #endif
 #ifdef OCRNL
-TTYMODE(OCRNL, c_oflag, "ocrnl")
+TTYMODE(OCRNL,   c_oflag, "ocrnl")
 #endif
 #ifdef ONOCR
-TTYMODE(ONOCR, c_oflag, "onocr")
+TTYMODE(ONOCR,   c_oflag, "onocr")
 #endif
 #ifdef ONLRET
-TTYMODE(ONLRET, c_oflag, "onlret")
+TTYMODE(ONLRET,  c_oflag, "onlret")
 #endif
 
-TTYMODE(CS7, c_cflag, "cs7")
-TTYMODE(CS8, c_cflag, "cs8")
-TTYMODE(PARENB, c_cflag, "parenb")
-TTYMODE(PARODD, c_cflag, "parodd")
+TTYMODE(CS7,     c_cflag, "cs7")
+TTYMODE(CS8,     c_cflag, "cs8")
+TTYMODE(PARENB,  c_cflag, "parenb")
+TTYMODE(PARODD,  c_cflag, "parodd")
 
 /* name, field, atom */
 #ifdef c_ispeed
