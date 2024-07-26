cdb49e4b725e5fd447afa8d0572a9d20b05069dd
Ed Wildgoose <ed+git@wildgooses.com>
2024-07-26 17:12:41 +0100

    Propogate capabilities to child process

--- c_src/exec_impl.cpp
+++ c_src/exec_impl.cpp
@@ -365,6 +365,55 @@ static int getpty(int& fdmp, ei::StringBuffer<128>& err) {
 }
 
 //------------------------------------------------------------------------------
+void propagate_caps(ei::StringBuffer<128>& err)
+{
+#ifdef HAVE_CAP
+    cap_t caps;
+    cap_value_t cap_list[CAP_LAST_CAP + 1];
+    int num_caps=0;
+
+    // Get the current process capabilities
+    caps = cap_get_proc();
+    if (caps == NULL) {
+        err.write("error %d on cap_get_proc: %s\n", errno, strerror(errno));
+    }
+
+    // Get the bounding set of capabilities
+    cap_flag_value_t cap_value;
+    for (cap_value_t i = 0; i <= CAP_LAST_CAP; ++i) {
+        if (cap_get_flag(caps, i, CAP_PERMITTED, &cap_value) == -1) {
+            err.write("error %d on cap_get_flag: %s\n", errno, strerror(errno));
+        }
+        if (cap_value == CAP_SET) {
+            cap_list[num_caps++] = i;
+        }
+    }
+
+    // Set inheritable capabilities to all permitted capabilities
+    if (cap_set_flag(caps, CAP_INHERITABLE, num_caps, cap_list, CAP_SET) == -1) {
+        err.write("error %d on cap_set_flag: %s\n", errno, strerror(errno));
+    }
+
+    // Apply inheritable capabilities
+    if (cap_set_proc(caps) == -1) {
+        err.write("error %d on cap_set_proc: %s\n", errno, strerror(errno));
+    }
+
+    // Set ambient capabilities
+    for (size_t i = 0; i < num_caps; i++) {
+        if (prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_RAISE, cap_list[i], 0, 0) == -1) {
+            err.write("error %d on PR_CAP_AMBIENT_RAISE: %s\n", errno, strerror(errno));
+        }
+    }
+
+    // Free the capability state
+    if (cap_free(caps) == -1) {
+        err.write("error %d on cap_free: %s\n", errno, strerror(errno));
+    }
+#endif
+}
+
+//------------------------------------------------------------------------------
 pid_t start_child(CmdOptions& op, std::string& error)
 {
     enum { RD = 0, WR = 1 };
@@ -489,7 +538,6 @@ pid_t start_child(CmdOptions& op, std::string& error)
     } else if (pid == 0) {
         // I am the child
         int r;
-        
         if (op.pty()) {
             int fds;
             char pts_name[256];
@@ -646,6 +694,8 @@ pid_t start_child(CmdOptions& op, std::string& error)
                 fprintf(stderr, "  CEnv: %s\r\n", p);
         }
         */
+
+        propagate_caps(err);
 
         const char* executable = op.executable().empty()
             ? argv[0] : op.executable().c_str();
